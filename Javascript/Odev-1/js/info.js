f4bbadca7879d2ba611de4cfda31844156c2b0b3
Mehmet Duran Kaya <44356848+mehmetdurankaya@users.noreply.github.com>
2023-09-03 14:53:26 +0300

    Update info.js

--- Javascript/Odev-1/js/info.js
+++ Javascript/Odev-1/js/info.js
@@ -1,1 +1,27 @@
+let fullName = prompt("Adınız Nedir?")
+if(fullName==""){
+    fullName="Mehmet Duran Kaya"
+}else{
+    fullName
+}
+let myName = document.querySelector('#myName')
+myName.innerHTML= fullName
 
+let selectSection=prompt("web development patikasının hangi bölümündesiniz")
+if(selectSection==""){
+    selectSection="Javascript"
+}else{
+    selectSection
+}
+let section=document.querySelector('#section')
+section.innerHTML=selectSection
+
+let selectWhich=prompt("Kaçıncı Ödeviniz")
+if(selectWhich==""){
+    selectWhich="1."
+}else{
+    selectWhich
+}
+
+let which=document.querySelector('#which')
+which.innerHTML=selectWhich
